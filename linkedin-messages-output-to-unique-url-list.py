cc9c94dbdea4665690bbde03ecd42e63636c9bc5
jasontoups <me@jasontoups.com>
2024-05-29 12:58:33 -0700

    moved source & output to variables

--- linkedin-messages-output-to-unique-url-list.py
+++ linkedin-messages-output-to-unique-url-list.py
@@ -7,8 +7,14 @@ linkedin_urls = set()
 # URL to exclude
 url_to_exclude = 'https://www.linkedin.com/in/jasontoups'
 
+# Source File: 
+source = 'messages-short.csv'
+
+# Output File:
+output = 'linkedin_urls-short.csv'
+
 # Open the CSV file
-with open('messages.csv', 'r') as f:
+with open(source, 'r') as f:
   reader = csv.reader(f)
   for row in reader:
     # Extract LinkedIn URLs from the 4th and 6th columns
@@ -24,7 +30,7 @@ with open('messages.csv', 'r') as f:
           linkedin_urls.add(url)
 
 # Write the unique LinkedIn URLs to a new CSV file
-with open('linkedin_urls.csv', 'w') as f:
+with open(output, 'w') as f:
   writer = csv.writer(f)
   for url in linkedin_urls:
     writer.writerow([url])
